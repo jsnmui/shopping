8bee5bf507b5e105222b5ad31e5cd9d722bff5d7
Jason Mui <jasonmui2004@gmail.com>
2026-01-10 10:54:20 -0500

    Changed base urls to render

--- shopping-app-frontend/src/environments/environment.prod.ts
+++ shopping-app-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  authApiUrl: 'https://shopping-production-7072.up.railway.app',
-  contentApiUrl: 'https://merry-wisdom-production.up.railway.app'
+  authApiUrl: 'https://shoppingapp-auth.onrender.com',
+  contentApiUrl: 'https://shoppingapp-content.onrender.com'
 
 };
